d3a83a62b527068bdfe6dbc3467a971ac2ee4718
KferrandonFulbert <98470782+kferrandonFulbert@users.noreply.github.com>
2024-11-20 16:10:36 +0100

    add PWA

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+      caches
+        .open("v1")
+        .then((cache) =>
+          cache.addAll([
+            "/",
+            "/index.html"
+          ]),
+        ),
+    );
+  });
+  
